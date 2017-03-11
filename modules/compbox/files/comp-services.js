31155befafc1b4d70477a511d6a0eaf1db7e1565
Peter Law <PeterJCLaw@gmail.com>
2017-03-11 14:35:37 +0000

    Use newly built-in mechanism for checking streams
    Change-Id: I63543845e0f5d55256935ec7d92add9fe4b381c8

--- modules/compbox/files/comp-services.js
+++ modules/compbox/files/comp-services.js
@@ -3,24 +3,7 @@ var mainWebsite = 'https://www.studentrobotics.org';
 var http = require('http');
 var request = require('request');
 
-addWatcher('compbox stream', function(ack, err) {
-    var req = http.request({'hostname': 'localhost',
-                            'path': '/stream'}, function(res) {
-        if (res.statusCode !== 200) {
-            err(res.statusCode);
-        } else if (res.headers['content-type'] !== 'text/event-stream') {
-            err("Bad content type: " + res.headers['content-type']);
-        } else {
-            req.abort();
-            ack();
-        }
-    });
-
-    req.on('error', function(e) {
-        err(e.message);
-    });
-    req.end();
-});
+addWatcher('compbox stream', watchStream('http://localhost/stream', ['team', 'ping']));
 
 addWatcher('compbox API', watchHTTP('http://localhost/comp-api/arenas'));
 
