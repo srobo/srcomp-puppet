a04216284a2311ebb4ee90b179c849bf4f32fc84
Peter Law <PeterJCLaw@gmail.com>
2017-03-13 23:15:43 +0000

    Add helper scripts from srcomp-kiosk
    Change-Id: Ia1972758dda7ebf2ba9613d5937c4a624d8f261f

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]
+then
+    echo "Must be run as root!"
+    exec sudo $0
+fi
+
+mydir=`dirname $0`
+cd $mydir
+mydir=$PWD
+
+puppet_dir='/etc/puppet'
+
+if [[ -d $puppet_dir ]]
+then
+    echo -n "Puppet dir ($puppet_dir) already exists, remove it? [y/N]: "
+    read do_remove
+    if [[ $do_remove = "y" ]]
+    then
+        rm -rf $puppet_dir
+    else
+        exit 1
+    fi
+fi
+
+ln -s $mydir $puppet_dir
+
+apt-get install puppet
+
+./deploy.sh
