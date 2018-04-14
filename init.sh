2bb5f788045702072e703e1e69e848e4f02e1950
Peter Law <PeterJCLaw@gmail.com>
2018-04-14 19:53:55 +0100

    Improve scripts

--- init.sh
+++ init.sh
@@ -6,28 +6,8 @@ then
     exec sudo $0
 fi
 
-mydir=`dirname $0`
-cd $mydir
-mydir=$PWD
-
-puppet_dir='/etc/puppet'
-
-if [[ -d $puppet_dir ]]
-then
-    echo -n "Puppet dir ($puppet_dir) already exists, remove it? [y/N]: "
-    read do_remove
-    if [[ $do_remove = "y" ]]
-    then
-        rm -rf $puppet_dir
-    else
-        exit 1
-    fi
-fi
+apt-get install --yes puppet git
 
 git submodule update --init --recursive
 
-ln -s $mydir $puppet_dir
-
-apt-get install puppet
-
-./deploy.sh
+exec ./apply-puppet.sh
