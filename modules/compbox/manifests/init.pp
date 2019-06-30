042a80e22032bbf74c92fec2682cfc067dd38d38
Peter Law <PeterJCLaw@gmail.com>
2019-06-30 14:54:23 +0100

    Revert "Switch back to using pip from PyPI via easy_install"
    This reverts commit 5a7aa6c4c963975366124cf145eb6ce55e52822f.
    
    easy_install3 isn't available on recent Ubuntus, however since
    Raspbian Buster has a more recent version of pip in the repos
    we can use that instead.

--- modules/compbox/manifests/init.pp
+++ modules/compbox/manifests/init.pp
@@ -143,6 +143,7 @@ class compbox {
                'python3-paramiko',
                'python3-pil',
                'python3-reportlab',
+               'python3-requests',
                'python3-ruamel.yaml',
                'python3-six']:
         ensure => present,
@@ -150,19 +151,13 @@ class compbox {
     }
 
     package { ['git',
+               'python3-pip',
                'python3-setuptools',
                'python3-dev',
                'python3-simplejson',
                'python3-sphinx',
                'python3-yaml']:
         ensure => present
-    } ->
-    # Raspbians's system pip is unreliable (specifically it experiences a
-    # TypeError if it needs to retry a download).
-    exec { 'install pip':
-        # `--upgrade` to encourage easy_install to get a fresh copy from PyPI.
-        command => '/usr/bin/easy_install3 --upgrade pip',
-        creates => '/usr/local/bin/pip3',
     } ->
     package { 'sr.comp.ranker':
         ensure   => $vcs_ensure,
@@ -183,7 +178,7 @@ class compbox {
         ensure   => $vcs_ensure,
         provider => 'pip3',
         source   => "git+${comp_source}/srcomp-cli.git",
-        require  => [Package['sr.comp'], Exec['install pip']],
+        require  => Package['sr.comp']
     }
 
     # Yaml loading acceleration
@@ -307,7 +302,7 @@ class compbox {
     package { 'gunicorn':
         ensure   => present,
         provider => 'pip3',
-        require  => Exec['install pip'],
+        require  => Package['python3-pip']
     }
     $compapi_logging_ini = '/var/www/srcomp-http-logging.ini'
     file { $compapi_logging_ini:
