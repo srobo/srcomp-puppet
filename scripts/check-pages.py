fa0999312c5ae1cba64fd37b8304a7f4cf53975d
Peter Law <PeterJCLaw@gmail.com>
2021-01-16 01:40:07 +0000

    Modernise the check script

--- scripts/check-pages.py
+++ scripts/check-pages.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
+#!/usr/bin/env python3
 
 import argparse
-from BeautifulSoup import BeautifulSoup
 import sys
-import urllib2
+
+import urllib.request
+from bs4 import BeautifulSoup
 
 DEFAULT_URL = 'http://localhost:8080'
 FAIL = '\033[91m'
@@ -21,12 +20,12 @@ parser.add_argument(
 args = parser.parse_args()
 
 try:
-    html_page = urllib2.urlopen(args.url)
+    html_page = urllib.request.urlopen(args.url)
 except Exception as e:
     print(FAIL, "Failed to load index page:", e, ENDC)
     exit(1)
 
-soup = BeautifulSoup(html_page)
+soup = BeautifulSoup(html_page, 'html.parser')
 
 fail_count = 0
 for link in soup.findAll('a'):
@@ -35,7 +34,7 @@ for link in soup.findAll('a'):
     print("Checking {0}...".format(desc), end=' ')
     sys.stdout.flush()
     try:
-        urllib2.urlopen(args.url + href)
+        urllib.request.urlopen(args.url + href)
     except Exception as e:
         print(FAIL, e, ENDC)
         fail_count += 1
