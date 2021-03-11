f6014794133567b3abd038cdb41f352a51f00962
Will Barber <WillB97@users.noreply.github.com>
2021-03-11 00:42:22 +0000

    IPv6 uses ICMPv6 for pings not ICMP

--- modules/compbox/manifests/fw_pre.pp
+++ modules/compbox/manifests/fw_pre.pp
@@ -27,7 +27,7 @@ class compbox::fw_pre {
 
   # Default firewall rules (IPv6)
   firewall { '000 accept all icmp (v6)':
-    proto     => 'icmp',
+    proto     => 'icmpv6',
     action    => 'accept',
     provider  => 'ip6tables',
   }->
