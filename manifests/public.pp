d79c3af14f54fdbf618a8c343159722f31b93178
Peter Law <PeterJCLaw@gmail.com>
2023-03-16 21:51:45 +0000

    Drop warning we don't need any more
    This has been fixed by ensuring the install ordering.
    See a965461e97f69b4492d99aae673a67cdf0ee8af9.

--- manifests/public.pp
+++ manifests/public.pp
@@ -4,7 +4,6 @@ node default {
         configure_main_user_access  => false,
         # main_user                 => 'root',
         manual_npm_installs         => false,
-        # Note: you almost certainly want to change enable_tls to `false` on first-run
         enable_tls                  => true,
     }
 }
