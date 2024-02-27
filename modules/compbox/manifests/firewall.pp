ce46a2a11c3203f38483d899f2c476a1af1fec6f
Peter Law <PeterJCLaw@gmail.com>
2024-02-27 20:40:52 +0000

    Include the name Mythic know this by too

--- modules/compbox/manifests/firewall.pp
+++ modules/compbox/manifests/firewall.pp
@@ -52,7 +52,7 @@ class compbox::firewall {
   }
 
   # Mythic Beasts
-  firewall { '200 allow Mythic Beasts\' monitoring access (v6)':
+  firewall { '200 allow Mythic Beasts\' munin monitoring access (v6)':
     dport     => 4949,
     source    => '2a00:1098:0:80:1000::100',
     proto     => tcp,
